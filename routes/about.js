16c332e7f2c64cd2514b9c782b6bc733a4fd0751
rainboy <rainboylvx@qq.com>
2019-10-30 09:57:55 +0800

    [加入] about 路由

--- routes/about.js
+++ routes/about.js
@@ -1,7 +1,27 @@
 const router = require('koa-router')()
+const pathFn = require("path")
+const fs = require("fs")
 
-router.get('/about',async (req,next)=>{
-  req.body = 'about'
+const about_list=['readme.md','README.md']
+router.get('/about',async (ctx ,next)=>{
+  for ( let name of about_list){
+    let path = pathFn.join( global.config.localRespository,name)
+    if( fs.existsSync(path)){
+      let article = await Cache.get(`about`)
+      if( !article){
+        article = await bookSystem.Render.render(path)
+        Cache.set(article,config.cache_time)
+      }
+      let {head} = bookSystem.Scan.split(path)
+      await ctx.render('article',{
+        title:'关于',
+        article,
+        head
+      })
+      return
+    }
+  }
+  await next()
 })
 
 module.exports = router
